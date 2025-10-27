1155e30e24bbfb636f1a6b4cc2c38e69f225865f
jjanu <jjanu@YUNGJANUARY60>
2025-10-27 17:24:14 +0100

    Update ResourceManager.hpp: poprawki i testy kompilacji

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,33 @@
-#pragma once
 
+#pragma once
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* res; 
+
+public:
+    
+    ResourceManager() : res(new Resource()) {}
+
+    
+    ~ResourceManager() { delete res; }
+
+    
+    ResourceManager(const ResourceManager& other) : res(new Resource(*other.res)) {}
+
+    
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this == &other)
+            return *this;                         
+        Resource* tmp = new Resource(*other.res); 
+        delete res;                               
+        res = tmp;                                
+        return *this;                             
+    }
+
+   
+    double get() { return res->get(); }
 };
